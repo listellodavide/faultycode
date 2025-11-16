08c9d59d5c5b89ba28dea2f964c78993c6541786
Davide Listello <davide.listello@gmail.com>
2025-11-16 19:34:23 +0100

    this code is faulty and need to be fixed before be possible to merge, create another branch and fix it

---
+++ src/main/java/com/adiwave/faultycode/FaultycodeApplication.java
@@ -0,0 +1,13 @@
+package com.adiwave.faultycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaultycodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaultycodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adiwave/faultycode/FaultycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adiwave.faultycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaultycodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
